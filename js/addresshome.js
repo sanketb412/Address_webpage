da49f41c5e113eeeec16a8f2d568358db41f0534
sanketb412 <sanketbagde38@gmail.com>
2021-06-21 10:23:27 +0530

    Added: code to populate the detials in table of home page

---
+++ js/addresshome.js
@@ -0,0 +1,49 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    createInnerHtml();
+});
+
+const createInnerHtml = () => {
+    const headerHtml =
+        "<tr><th>Fullname</th><th>Address</th><th>City</th><th>State</th><th>Zip Code</th><th>PhoneNumber</th><th>Actions</th></tr>";
+ let contactList = createContactDetailsJSON();
+  let innerHtml = `${headerHtml}`;
+  for(const contact of contactList){
+     innerHtml = `${innerHtml}
+     <tr>
+           <td>${contact._name}</td>
+             <td>${contact._address}</td>
+             <td>${contact._city}</td>
+             <td>${contact._state}</td>
+             <td>${contact._zip}</td>
+             <td>${contact._phoneNumber}</td>
+             <td>
+                 <img id="${contact._id}" onclick="remove(this)"  src="../assets/icons/delete-black-18dp.svg" alt="delete">
+                 <img id="${contact._id}" onclick="update(this)"  src="../assets/icons/create-black-18dp.svg" alt="edit">
+             </td>
+         </tr>
+     `;
+}
+document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const createContactDetailsJSON = () => {
+    let contactListLocal = [
+        {
+            _name:'Sanket',
+            _address:'Mulund',
+            _city:'Mumbai',
+            _state:'Maharashtra',
+            _zip:'400080',
+            _phoneNumber:'85463221445',
+        },
+        {
+           _name:'Vicky',
+           _address:'Mulund',
+           _city:'Mumbai',
+           _state:'Maharastra',
+           _zip:'400081',
+           _phoneNumber:'8546231477',
+        }
+    ]
+    return contactListLocal;
+   } 
